a1b597c71afb40aef721dbf90c4139a8421dd14e
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-09-16 01:33:39 +0900

    Update meson.sh

--- meson.sh
+++ meson.sh
@@ -43,8 +43,11 @@ echo -e "${GREEN}https://dashboard.gaganode.com/install_run 해당 사이트에 
 read -p "위 사이트에서 복사한 토큰 ID를 입력해주세요: " TOKEN_ID
 
 # 입력받은 토큰 ID를 사용하여 명령어를 실행합니다.
-req "토큰 ID를 사용하여 Gaganode 설정을 적용합니다." sudo ./apps/gaganode/gaganode config set --token="$TOKEN_ID"
-req "서비스가 OK로 출력되는지 확인하세요." sudo ./apphub status
+echo -e "${YELLOW}토큰 ID를 사용하여 Gaganode 설정을 적용합니다.${NC}"
+sudo ./apps/gaganode/gaganode config set --token="$TOKEN_ID"
+
+# 서비스 상태를 다시 확인합니다.
+req "서비스가 OK로 출력되는지 다시 한번 확인하세요." sudo ./apphub status
 
 echo -e "${GREEN}모든 작업이 완료되었습니다. 컨트롤+A+D로 스크린을 종료해주세요.${NC}"
 echo -e "${GREEN}스크립트 작성자: https://t.me/kjkresearch${NC}"
